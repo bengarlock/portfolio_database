e903ab9c585987c476d675268b2715ae2df9f02c
Ben Garlock <ben.garlock@gmail.com>
2020-12-16 17:35:26 -0500

    updated task model to allow for blank values in notes

--- task/models.py
+++ task/models.py
@@ -7,7 +7,7 @@ from folder.models import Folder
 class Task(models.Model):
     date = models.TextField(max_length=50, default=date.today())
     name = models.TextField(max_length=50, default="Task Name")
-    notes = models.TextField(max_length=500, default="")
+    notes = models.TextField(max_length=500, default="", blank=True)
     status = models.TextField(max_length=50, default="pending")
     folder_id = models.ForeignKey(Folder, related_name="tasks", on_delete=models.CASCADE, default='')
 
