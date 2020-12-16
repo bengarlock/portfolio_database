77ef13eaa73b75a0910a1d5f8b79588499cb05eb
Ben Garlock <ben.garlock@gmail.com>
2020-12-16 12:22:58 -0500

    removed update in guest

--- guests/serializers.py
+++ guests/serializers.py
@@ -17,21 +17,21 @@ class GuestSerializer(serializers.ModelSerializer):
         model = Guest
         fields = ['id', 'first_name', 'last_name', 'phone_number', 'guest_notes', 'root_user', 'slots']
 
-    def update(self, instance, validated_data):
-
-        bad_chars = [';', ':', '!', "*", ",", ".", "-", "+1"]
-        for i in bad_chars:
-            phone_number = phone_number.replace(i, '')
-
-        validated_data["phone_number"] = phone_number
-
-        if instance.root_user:
-            pass
-        else:
-            instance.first_name = validated_data.get('first_name', instance.first_name)
-            instance.last_name = validated_data.get('last_name', instance.last_name)
-            instance.phone_number = validated_data.get('phone_number', instance.phone_number)
-            instance.guest_notes = validated_data.get('guest_notes', instance.guest_notes)
-            instance.save()
-        return instance
+    # def update(self, instance, validated_data):
+    #
+    #     bad_chars = [';', ':', '!', "*", ",", ".", "-", "+1"]
+    #     for i in bad_chars:
+    #         phone_number = phone_number.replace(i, '')
+    #
+    #     validated_data["phone_number"] = phone_number
+    #
+    #     if instance.root_user:
+    #         pass
+    #     else:
+    #         instance.first_name = validated_data.get('first_name', instance.first_name)
+    #         instance.last_name = validated_data.get('last_name', instance.last_name)
+    #         instance.phone_number = validated_data.get('phone_number', instance.phone_number)
+    #         instance.guest_notes = validated_data.get('guest_notes', instance.guest_notes)
+    #         instance.save()
+    #     return instance
 
