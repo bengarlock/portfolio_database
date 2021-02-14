9768504785049df0257fe19ee344211d1e798f2b
Ben Garlock <ben.garlock@gmail.com>
2021-02-14 09:48:53 -0500

    server commit

--- okta_interview/views.py
+++ okta_interview/views.py
@@ -3,6 +3,9 @@ from rest_framework import serializers, viewsets
 from .models import OktaInterview
 import requests
 from passwords import return_api_key
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
 
 
 def sync_okta():
@@ -18,6 +21,7 @@ def sync_okta():
 
     response = requests.request("GET", url, data=payload, headers=headers)
     response_json = response.json()
+    return response.json()
 
     for item in response_json:
         if OktaInterview.objects.filter(oktaid=item["id"]):
@@ -56,3 +60,60 @@ class OktaInterviewSerializer(serializers.ModelSerializer):
 class OktaInterviewView(viewsets.ModelViewSet):
     queryset = OktaInterview.objects.all()
     serializer_class = OktaInterviewSerializer
+
+    def get_queryset(request, format=None):
+        url = "https://dev-49794790.okta.com/api/v1/users"
+        api_token = return_api_key()
+
+        payload = ""
+        headers = {
+            'Accept': "application/json",
+            'Content-Type': "application/json",
+            'Authorization': 'SSWS' + api_token
+        }
+
+        request = requests.request("GET", url, data=payload, headers=headers)
+        response = request.json()
+        print(response)
+
+        for item in response:
+            first_name = item["profile"]["firstName"]
+            last_name = item["profile"]["lastName"]
+            mobile = item["profile"]["mobilePhone"]
+            display_name = None
+            second_email = item["profile"]["secondEmail"]
+            login = item["profile"]["login"]
+            email = item["profile"]["email"]
+            oktaid = item["id"]
+
+
+            if OktaInterview.objects.filter(oktaid=item["id"]):
+
+                print("User {} in database".format(item["id"]))
+                user = OktaInterview.objects.get(oktaid=item["id"])
+                user.firstName = first_name
+                user.lastName = last_name
+                user.mobilePhone = mobile
+                user.displayName = display_name
+                user.secondEmail = second_email
+                user.login = login
+                user.email = email
+                user.save()
+
+            else:
+                OktaInterview.objects.create(
+                    firstName=first_name,
+                    lastName=last_name,
+                    mobilePhone=mobile,
+                    displayName=display_name,
+                    secondEmail=second_email,
+                    login=login,
+                    email=email,
+                    oktaid=oktaid,
+                )
+
+
+        users = OktaInterview.objects.all()
+        return users
+
+
