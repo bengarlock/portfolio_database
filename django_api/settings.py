d29eca6460dea63ca9d7805d775c5b924e99c8b2
Ben Garlock <ben.garlock@gmail.com>
2021-03-05 11:33:18 -0500

    cleanup

--- django_api/settings.py
+++ django_api/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = return_secret_key()
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'http://localhost:3000/',
+    'http://localhost:3000',
     'www.bengarlock.com',
     'https://www.bengarlock.com:6320',
     'https://www.bengarlock.com:6321',
@@ -84,7 +84,7 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000/',
+    'http://localhost:3000',
     'https://www.bengarlock.com:6320',
     'https://www.bengarlock.com:6321',
     'https://www.bengarlock.com:6322',
